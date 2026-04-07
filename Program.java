eec8ac62cdc85d0ed0a83f3fd1ad6c4cdac1a87f
Kurash1 <clasheroff7@gmail.com>
2026-04-07 14:56:31 +0100

    Quick Adjustment

--- Program.java
+++ Program.java
@@ -2,26 +2,31 @@ import javax.swing.*;
 
 public class Program
 {
-    static Boolean isRunning = true;
-    static long lastTime;
-    
-    public static void Main() {
-        SwingUtilities.invokeLater(() -> { _start(); });
-    }
-    
     public static void _start()
     {
-        DrawingSystem._start();
         TestingField._start();
+        DrawingSystem._start();
         
         lastTime = System.nanoTime();
         GameLoop();
     }
     
     public static void _stop() {
-        DrawingSystem._stop();
         TestingField._stop();
-        System.exit(0);
+        DrawingSystem._stop();
+    }
+    
+    public static void _process(double delta)
+    {
+        TestingField._process(delta);
+        DrawingSystem._process(delta);
+    }
+    
+    static Boolean isRunning = true;
+    static long lastTime;
+    
+    public static void Main() {
+        SwingUtilities.invokeLater(() -> { _start(); });
     }
 
     public static void GameLoop() {
@@ -34,13 +39,7 @@ public class Program
         double delta = (now - lastTime) / 1_000_000_000.0;
         lastTime = now;
         
-        Process(delta);
+        _process(delta);
         SwingUtilities.invokeLater(Program::GameLoop);
-    }
-    
-    public static void Process(double delta)
-    {
-        DrawingSystem._process(delta);
-        TestingField._process(delta);
     }
 }
