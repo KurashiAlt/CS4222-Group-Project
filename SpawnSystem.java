801ef4cf6e2d3d27c42a2331ebd14f70ae23d275
HanMing07 <25467816@studentmail.ul.ie>
2026-04-28 17:31:12 +0100

    Update SpawnSystem.java
    random enemy spawn

--- SpawnSystem.java
+++ SpawnSystem.java
@@ -1,12 +1,16 @@
 public class SpawnSystem
 {
     public static double SpawnTimer = 0;
-    public static double SPAWN_RATE = 1; 
+    public static double SPAWN_RATE = 1.9; 
     public static void _process(double delta) {
         SpawnTimer += delta;
         if (SpawnTimer > SPAWN_RATE) {
             SpawnTimer = 0;
-            EnemySystem.spawnEnemy(new Vector2(200, 0));
+            int minX = 50;
+            int maxX = 750;
+            
+            int randomX=minX + (int)(Math.random()*(maxX-minX));
+            EnemySystem.spawnEnemy(new Vector2(randomX, 0));
         }
     }
 }
